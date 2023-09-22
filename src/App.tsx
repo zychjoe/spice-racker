e2696132ca2612eee2b6e705990b4419bcada636
Joe Zych <zychjoe@gmail.com>
2023-09-21 20:03:34 -0400

    basic data interfaces created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+interface LibrarySpice {
+  name: String,
+  shelfLife: Number | null,
+  image: null,
+}
+
+interface InventorySpice {
+  spice: LibrarySpice,
+  expDate: Date | null,
+}
+
+const beginningLibrary: Array<LibrarySpice> = [
+  {
+    name:  "Cilantro",
+    shelfLife: null,
+    image: null,
+  },
+  {
+    name:  "Oregano",
+    shelfLife: null,
+    image: null,
+  },
+  {
+    name:  "Cinnamon",
+    shelfLife: null,
+    image: null,
+  },
+
+]
+
+const beginningInventory : Array<InventorySpice> = [
+  {
+    spice: beginningLibrary[1],
+    expDate: null
+  }
+]
+
 function App() {
+  const [ library, setLibrary ] = useState<Array<LibrarySpice>>(beginningLibrary);
+  const [ inventory, setInventory ] = useState<Array<InventorySpice>>(beginningInventory);
+
+
+
+  console.log(library);
+  console.log(inventory);
+
   return (
     <div className="App">
       <header className="App-header">
