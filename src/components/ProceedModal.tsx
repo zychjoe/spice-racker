278233725a6b71927203bc96628d36e883a63f19
Joe Zych <zychjoe@gmail.com>
2023-09-30 22:47:59 -0400

    proceed modal open

---
+++ src/components/ProceedModal.tsx
@@ -0,0 +1,26 @@
+import './ProceedModal.css'
+import SpiceyBtn from './SpiceyBtn';
+
+interface modalProps {
+  prompt: string,
+  cancelFunc: (event: React.MouseEvent<HTMLButtonElement>) => void,
+  cancelText?: string,
+  submitFunc: (event: React.MouseEvent<HTMLButtonElement>) => void,
+  submitText?: string
+}
+
+function ProceedModal(props: modalProps) {
+
+  const { prompt, cancelFunc, cancelText, submitFunc, submitText } = props;
+
+  return(
+    <div>
+      <p>{prompt}</p>
+      <SpiceyBtn onClick={cancelFunc} btnText={cancelText? cancelText : 'Cancel'} />
+      <SpiceyBtn onClick={submitFunc} btnText={submitText? submitText : 'Submit'} />
+    </div>
+  )
+
+}
+
+export default ProceedModal;
